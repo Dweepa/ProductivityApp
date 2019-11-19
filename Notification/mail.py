54338dbb2fa1e51f255d53f8c9338dfc800c230d
Dweepa <dweepa.prasad@gmail.com>
2019-11-19 12:47:56 +0530

    Added mail functionality

--- Notification/mail.py
+++ Notification/mail.py
@@ -11,54 +11,58 @@ app = Flask(__name__)
 scheduler = BackgroundScheduler({'apscheduler.timezone': 'Asia/Calcutta'})
 scheduler.start()
 
-
 mail_settings = {
     "MAIL_SERVER": 'smtp.gmail.com',
     "MAIL_PORT": 465,
     "MAIL_USE_TLS": False,
     "MAIL_USE_SSL": True,
-    "MAIL_USERNAME": "xyz41607@gmail.com",
-    "MAIL_PASSWORD": "abcxyz12345"
+    "MAIL_USERNAME":"pesuprod@gmail.com",
+    "MAIL_PASSWORD":"pesu@123"
+    # "MAIL_USERNAME": "xyz41607@gmail.com",
+    # "MAIL_PASSWORD": "abcxyz12345"
 }
 
 app.config.update(mail_settings)
 mail = Mail(app)
-
 
 app.config['MONGO_DBNAME'] = 'student'
 app.config['MONGO_URI'] = "mongodb://localhost:27017/student"
 CORS(app)
 mongo = PyMongo(app)
 
+
 @app.route('/schedulePrint', methods=['POST'])
 def schedule_to_print():
-    #get time to schedule and text to print from the json
-    #time = "2019-11-13 21:50:00"
-    #convert to datetime
+    # get time to schedule and text to print from the json
+    # time = "2019-11-13 21:50:00"
+    # convert to datetime
     student = mongo.db.student
     data = request.get_json(force=True)
+    print(data)
     time = data["time"]
-    sec = data["section"] 
+    body = data["body"]
+    sec = data["section"]
     docs = list(student.find({"section": sec}))
-
+    print(docs)
     output = []
     for i in docs:
-        
         output.append(i['email'])
         print(i)
-    
+
     date_time = datetime.strptime(str(time), '%Y-%m-%d %H:%M:%S')
+    print(date_time)
     print(output)
-    job = scheduler.add_job(notification, trigger='date', next_run_time=str(date_time),args = [output])
+    job = scheduler.add_job(notification, trigger='date', next_run_time=str(date_time), args=[output,body])
     return "job details: %s" % job
 
 
-def notification(output):
-	print(output)
-	with app.app_context():
-            msg = Message(subject="Hello",sender=app.config.get("MAIL_USERNAME"),recipients = output,body="Today is the deadline for your assignment !")
-            mail.send(msg)
+def notification(output,body):
+    print(output)
+    with app.app_context():
+        msg = Message(subject="Hello", sender=app.config.get("MAIL_USERNAME"), recipients=output,
+                      body="Today is the deadline for your assignment ! "+body)
+        mail.send(msg)
+
 
 if __name__ == '__main__':
-	
-	app.run(debug=True,port=2000)
+    app.run(debug=True, port=2000)
--- calendar-api.py
+++ calendar-api.py
@@ -7,12 +7,71 @@ import base64
 import datetime
 from flask_cors import CORS
 
+from flask import Flask, request
+from apscheduler.schedulers.background import BackgroundScheduler
+from datetime import datetime
+from flask_mail import Mail, Message
+
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'calendar'
 app.config['MONGO_URI'] = "mongodb://localhost/calendar"
 CORS(app)
 mongo = PyMongo(app)
 
+scheduler = BackgroundScheduler({'apscheduler.timezone': 'Asia/Calcutta'})
+scheduler.start()
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME":"pesuprod@gmail.com",
+    "MAIL_PASSWORD":"pesu@123"
+    # "MAIL_USERNAME": "xyz41607@gmail.com",
+    # "MAIL_PASSWORD": "abcxyz12345"
+}
+
+app.config.update(mail_settings)
+mail = Mail(app)
+
+app.config['MONGO_DBNAME'] = 'student'
+app.config['MONGO_URI'] = "mongodb://localhost:27017/student"
+CORS(app)
+mongo = PyMongo(app)
+
+
+# @app.route('/schedulePrint', methods=['POST'])
+def schedule_to_print(data):
+    # get time to schedule and text to print from the json
+    # time = "2019-11-13 21:50:00"
+    # convert to datetime
+    student = mongo.db.student
+    # data = request.get_json(force=True)
+    print(data)
+    time = data["time"]
+    body = data["body"]
+    sec = data["section"]
+    docs = list(student.find({"section": sec}))
+    print(docs)
+    output = []
+    for i in docs:
+        output.append(i['email'])
+        print(i)
+
+    date_time = datetime.strptime(str(time), '%Y-%m-%d %H:%M:%S')
+    print(date_time)
+    print(output)
+    job = scheduler.add_job(notification, trigger='date', next_run_time=str(date_time), args=[output,body])
+    return "job details: %s" % job
+
+
+def notification(output,body):
+    print(output)
+    with app.app_context():
+        msg = Message(subject="Hello", sender=app.config.get("MAIL_USERNAME"), recipients=output,
+                      body="Today is the deadline for your assignment ! "+body)
+        mail.send(msg)
 
 @app.route('/show/<date>/<month>/<year>', methods=['GET'])
 def show(date, month, year):
@@ -30,7 +89,7 @@ def show(date, month, year):
     return jsonify({"output": output})
 
 
-@app.route('/add', methods=['GET', 'POST'])
+@app.route('/add', methods=['POST'])
 def add():
     calendar = mongo.db.calendar
     req = request.json
@@ -38,8 +97,16 @@ def add():
     event_dict = {'type': req['title'], 'course': req['course'],
                   'section': req['section'], 'team': req['team'],
                   'description': req['description']}
-    uid = calendar.insert({'D': int(req['d']), 'M': req['m'], 'Y': int(req['y']),'event':event_dict })
+    uid = calendar.insert({'D': int(req['d']), 'M': req['m'], 'Y': int(req['y']), 'event': event_dict})
     print(uid)
+
+    req_dict = dict()
+    req_dict['section'] = req['section']
+    req_dict['body'] = req['description']
+    date = str(req['y']) + "-" + str(req['m']) + "-" + str(req['d']) + " " + "08:00:00"
+    print(date)
+    req_dict['time'] = (datetime.strptime(date, '%Y-%B-%d %H:%M:%S')).strftime("%Y-%m-%d %H:%M:%S")
+    print(schedule_to_print(req_dict))
     # op = list(calendar.find({"_id":uid}))
     return jsonify({"output": 'insert done'})
 
